0f67f27d3ee6d97874765d4eaad290d4c8846e90
Mohammed-Hayatudeen Yusuf Anate <mhibnyusufanate@gmail.com>
2026-02-24 14:28:27 +0100

    feat:fixError

---
+++ src/app/error.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+// ... rest of your error.tsx file
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <button onClick={() => reset()}>Try again</button>
+    </div>
+  );
+}
